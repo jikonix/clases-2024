d80d82f437f0cbbd213f57774e13108b893716b1
jikonix <127434705+jikonix@users.noreply.github.com>
2024-08-21 22:14:53 -0400

    ionic login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+
 ];
